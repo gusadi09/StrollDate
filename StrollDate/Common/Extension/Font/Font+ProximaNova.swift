cd66b4e9847dfd58fb4c90c978d044ee010c084c
AgusAdiPranata <agus@ewideplus.com>
2025-06-19 10:23:47 +0700

    [INIT] create custom font caller via extension swiftui font

--- StrollDate/Common/Extension/Font/Font+ProximaNova.swift
+++ StrollDate/Common/Extension/Font/Font+ProximaNova.swift
@@ -5,4 +5,20 @@
 //  Created by Ewide Dev 5 on 19/06/25.
 //
 
-import Foundation
+import SwiftUI
+
+extension Font {
+    enum ProximaNovaStyle: String {
+        case black = "proximanova_black"
+        case blackItalic = "proximanova_blackit"
+        case bold = "proximanova_bold"
+        case boldItalic = "proximanova_boldit"
+        case extraBold = "proximanova_extrabold"
+        case light = "proximanova_light"
+        case regular = "proximanova_regular"
+    }
+    
+    static func proximaNova(_ type: ProximaNovaStyle, size: CGFloat) -> Font {
+        .custom(type.rawValue, size: size)
+    }
+}
