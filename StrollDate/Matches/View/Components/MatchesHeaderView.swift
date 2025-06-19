f91f36ded55489d13db25a5d18d47da26708610e
AgusAdiPranata <agus@ewideplus.com>
2025-06-19 10:14:51 +0700

    [INIT] add components folder for every view

---
+++ StrollDate/Matches/View/Components/MatchesHeaderView.swift
@@ -0,0 +1,18 @@
+//
+//  MatchesHeaderView.swift
+//  StrollDate
+//
+//  Created by Ewide Dev 5 on 19/06/25.
+//
+
+import SwiftUI
+
+struct MatchesHeaderView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MatchesHeaderView()
+}
